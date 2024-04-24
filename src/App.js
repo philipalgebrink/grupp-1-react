a7e6b1853fcf578dea2bb8065fca305e2ac2b6ae
Baranbulduk <baranbulduk99@gmail.com>
2024-04-24 11:07:27 +0200

    test commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './Header';
 import Button from './Button';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Grupp 1</h1>
       <Button />
     </div>
