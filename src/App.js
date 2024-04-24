ac93926efc01f23cdd4a39e5bcc49250407e7130
Baranbulduk <baranbulduk99@gmail.com>
2024-04-24 11:14:36 +0200

    filer och mapp tillagt

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
-import Header from './Header';
-import Button from './Button';
+import Header from './components/Header';
+import Card from './components/Card';
+import Form from './components/Form';
+import Button from './components/Button';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <h1>Grupp 1</h1>
+      <Card />
+      <Form />
       <Button />
     </div>
   );
---
+++ src/components/Form.jsx
@@ -0,0 +1,7 @@
+export default function Form() {
+    return(
+    <form>
+        <h1>Form</h1>
+    </form>
+    );
+}
