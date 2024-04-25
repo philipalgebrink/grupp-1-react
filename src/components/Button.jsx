5f40038d02bc59601d9bede8c051f241115a5b82
Emma Dybdorf <emma.dybdorf@hotmail.com>
2024-04-25 13:59:04 +0200

    Change Button.jsx och App.css btn

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,29 +1,9 @@
+import '../pages/App.css';
+
 function Button({ title }) {
     return (
-      <div style={{
-          display: 'flex', 
-          justifyContent: 'center', 
-          alignItems: 'center', 
-          margin: '20px',
-      }}>
-        <div style={{
-            border: "1px solid black",
-            padding: "20px",
-            textAlign: "center",
-            width: "382px",
-            height: "80px",
-            borderRadius: "8px",
-        }}>
-          <div style={{
-            fontFamily: 'PT Mono',
-            fontSize: '22px',
-            fontWeight: 700,
-            lineHeight: '24.64px',
-            textAlign: 'center'
-            }}>
-            {title}
-            </div>
-        </div>
+      <div className="btn">
+        {title}
       </div>
     );
 }
