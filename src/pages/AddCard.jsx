ebcab2b1265b10c805381614568425dfbd874088
Emma Dybdorf <emma.dybdorf@hotmail.com>
2024-04-26 12:09:25 +0200

    AddCard.jsx

--- src/pages/AddCard.jsx
+++ src/pages/AddCard.jsx
@@ -3,11 +3,13 @@ import Header from '../components/Header';
 import Card from '../components/Card';
 import Form from '../components/Form';
 import Button from '../components/Button';
+import SubHeader from '../components/SubHeader';
 
 function AddCard() {
   return (
     <div className="App">
       <Header title="ADD A NEW BANK CARD" />
+      <SubHeader title="NEW CARD"/>
       <Card />
       <Form />
       <Button title="ADD card"/>
