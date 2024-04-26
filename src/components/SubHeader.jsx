263c6df2e5a0c326dc2333c9f47b47ec993951fa
RockAnnRoll <144233838+RockAnnRoll@users.noreply.github.com>
2024-04-26 12:05:09 +0200

    Create SubHeader.jsx

---
+++ src/components/SubHeader.jsx
@@ -0,0 +1,11 @@
+import '../pages/App.css';
+
+function SubHeader({ title }) {
+    return (
+      <div className="subheader">
+        {title}
+      </div>
+    );
+}
+
+export default SubHeader;
