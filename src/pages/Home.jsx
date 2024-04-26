6c64488cf14c0079027f541b8a2b28de3e17a48b
Emma Dybdorf <emma.dybdorf@hotmail.com>
2024-04-26 11:02:51 +0200

    App.css, Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -58,7 +58,7 @@ const Home = () => {
         </div>
       </div>
       <Link to="/addcard">
-      <Button title="Add a new card" className="home-button" />
+      <Button title="Add a new card" className="home-button" />      
       </Link>
     </div>
   );
