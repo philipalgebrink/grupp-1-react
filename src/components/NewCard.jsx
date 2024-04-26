d55a5a82aa78e66cebe2f7e593c50d283426728f
Philip Älgebrink <76400643+philipalgebrink@users.noreply.github.com>
2024-04-26 12:33:49 +0200

    Added NewCard component template
    For Baran to inplement form info in to

---
+++ src/components/NewCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import chipLogo from '../assets/chiplogo.svg';
+import wifiLogo from '../assets/wifilogo.svg';
+
+const NewCard = ({ vendor, number, name }) => {
+    const cardClassName = `card ${vendor}`;
+
+    return (
+        <div className={cardClassName}>
+            <img src={wifiLogo} alt="wifi" className="wifilogo" />
+            <img src={chipLogo} alt="chip" className="chiplogo" />
+            <h1 className="number">{number || 'XXXX XXXX XXXX XXXX'}</h1>
+            <div className="namedate">
+                <div className="line1">
+                    <p>CARDHOLDER NAME</p>
+                    <p>VALID THRU</p>
+                </div>
+                <div className="line2">
+                    <p>{name || 'Cardholder Name'}</p>
+                    <p>12/24</p>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default NewCard;
--- src/pages/AddCard.jsx
+++ src/pages/AddCard.jsx
@@ -3,14 +3,15 @@ import Header from '../components/Header';
 import Card from '../components/Card';
 import Form from '../components/Form';
 import Button from '../components/Button';
+import NewCard from '../components/NewCard';
 
 function AddCard() {
   return (
     <div className="App">
       <Header title="ADD A NEW BANK CARD" />
-      <Card />
+      <NewCard />
       <Form />
-      <Button title="ADD card"/>
+      <Button title="ADD card" />
     </div>
   );
 }
